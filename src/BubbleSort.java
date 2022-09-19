0be3adec6669406f27600d0fe5c10d0c1a3cbad6
Adarsh Singh <bananaisnotafruit69@gmail.com>
2022-09-19 09:09:03 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,30 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int arr[] = {5,3,1,7,8,9};
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length-1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+				
+			}
+			
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
